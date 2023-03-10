1cd07dcde1517d8ebbc496efbc1feb93523bdf03
knight.chen <knightchen@knx.com.cn>
2023-03-11 02:10:27 +0800

    feat(naive-ui): 完成 avatar 和 card 的适配

--- packages/naive-ui/src/avatar/meta.ts
+++ packages/naive-ui/src/avatar/meta.ts
@@ -1,7 +1,86 @@
-import type { ComponentMetadata } from '@alilc/lowcode-types';
+import type { IPublicTypeComponentMetadata } from '@alilc/lowcode-types';
+import avatarImage from './__screenshots__/avatar-1.jpg?inline';
 
-const meta: ComponentMetadata = {
+const meta: IPublicTypeComponentMetadata = {
   componentName: 'NAvatar',
+  title: '头像',
+  category: '数据展示',
+  configure: {
+    props: [
+      {
+        name: 'src',
+        title: '头像的地址',
+        setter: 'StringSetter',
+      },
+      {
+        name: 'fallback-src',
+        title: {
+          label: '加载失败地址',
+          tip: '头像加载失败时显示的图片的地址',
+        },
+        setter: 'StringSetter',
+      },
+      {
+        name: 'color',
+        title: '头像的背景色',
+        setter: 'ColorSetter',
+      },
+      {
+        name: 'circle',
+        title: '是否为圆形',
+        setter: 'BoolSetter',
+      },
+      {
+        name: 'round',
+        title: '是否显示圆角',
+        setter: 'BoolSetter',
+      },
+      {
+        name: 'bordered',
+        title: '是否带边框',
+        setter: 'BoolSetter',
+      },
+      {
+        name: 'size',
+        title: '头像大小',
+        setter: {
+          componentName: 'MixedSetter',
+          props: {
+            setters: [
+              {
+                componentName: 'RadioGroupSetter',
+                props: {
+                  options: [
+                    { label: 'small', value: 'small' },
+                    { label: 'medium', value: 'medium' },
+                    { label: 'large', value: 'large' },
+                  ],
+                  defaultValue: 'medium',
+                },
+              },
+              'NumberSetter',
+            ],
+          },
+        },
+      },
+    ],
+    supports: {
+      style: true,
+      events: ['onClick', 'onError'],
+    },
+  },
+  snippets: [
+    {
+      title: '头像',
+      screenshot: avatarImage,
+      schema: {
+        componentName: 'NAvatar',
+        props: {
+          src: 'https://www.naiveui.com/assets/naivelogo-93278402.svg',
+        },
+      },
+    },
+  ],
 };
 
 export default meta;
--- packages/naive-ui/src/card/meta.ts
+++ packages/naive-ui/src/card/meta.ts
@@ -1,7 +1,210 @@
-import type { ComponentMetadata } from '@alilc/lowcode-types';
+import type { IPublicTypeComponentMetadata } from '@alilc/lowcode-types';
+import cardImage from './__screenshots__/card-1.png?inline';
 
-const meta: ComponentMetadata = {
+const meta: IPublicTypeComponentMetadata = {
   componentName: 'NCard',
+  title: '卡片',
+  category: '数据展示',
+  configure: {
+    supports: {
+      style: true,
+      events: ['onClose'],
+      loop: false,
+    },
+    component: {
+      isContainer: true,
+    },
+    props: [
+      {
+        type: 'group',
+        title: '基础设置',
+        extraProps: {
+          display: 'block',
+        },
+        items: [
+          {
+            name: 'size',
+            title: '卡片尺寸',
+            setter: {
+              componentName: 'RadioGroupSetter',
+              props: {
+                options: [
+                  { label: 'small', value: 'small' },
+                  { label: 'medium', value: 'medium' },
+                  { label: 'large', value: 'large' },
+                  { label: 'huge', value: 'huge' },
+                ],
+                defaultValue: 'medium',
+              },
+            },
+          },
+          {
+            name: 'bordered',
+            title: '显示边框',
+            setter: 'BoolSetter',
+          },
+          {
+            name: 'closable',
+            title: '显示关闭图标',
+            setter: 'BoolSetter',
+          },
+          {
+            name: 'hoverable',
+            title: '可悬浮',
+            setter: 'BoolSetter',
+          },
+        ],
+      },
+      {
+        type: 'group',
+        title: '卡片样式',
+        extraProps: {
+          display: 'block',
+        },
+        items: [
+          {
+            name: 'content-style',
+            extraProps: {
+              display: 'popup',
+            },
+            title: { label: '内容样式', tip: '卡片内容区域的样式' },
+            setter: 'StyleSetter',
+          },
+          {
+            name: 'footer-style',
+            extraProps: {
+              display: 'popup',
+            },
+            title: { label: '底部样式', tip: '卡片底部区域的样式' },
+            setter: 'StyleSetter',
+          },
+          {
+            name: 'header-style',
+            extraProps: {
+              display: 'popup',
+            },
+            title: { label: '头部样式', tip: '卡片头部区域的样式' },
+            setter: 'StyleSetter',
+          },
+          {
+            name: 'header-extra-style',
+            extraProps: {
+              display: 'popup',
+            },
+            title: { label: '头部额外内容样式', tip: '卡片头部额外内容的样式' },
+            setter: 'StyleSetter',
+          },
+        ],
+      },
+      {
+        type: 'group',
+        title: '分割线',
+        extraProps: {
+          display: 'block',
+        },
+        items: [
+          {
+            name: 'segmented.content',
+            title: '内容分割线',
+            setter: {
+              componentName: 'BoolSetter',
+              initialValue: false,
+            },
+          },
+          {
+            name: 'segmented.footer',
+            title: '底部分割线',
+            setter: {
+              componentName: 'BoolSetter',
+              initialValue: false,
+            },
+          },
+          {
+            name: 'segmented.action',
+            title: '操作区分割线',
+            setter: {
+              componentName: 'BoolSetter',
+              initialValue: false,
+            },
+          },
+        ],
+      },
+      {
+        title: '卡片内容',
+        type: 'group',
+        extraProps: {
+          display: 'block',
+        },
+        items: [
+          {
+            name: 'title',
+            title: '标题内容',
+            setter: 'StringSetter',
+          },
+          {
+            name: 'header',
+            title: '头部',
+            setter: {
+              componentName: 'SlotSetter',
+              initialValue: {
+                type: 'JSSlot',
+                title: '头部',
+                value: [],
+              },
+            },
+          },
+          {
+            name: 'header-extra',
+            title: '头部附加',
+            setter: {
+              componentName: 'SlotSetter',
+              initialValue: {
+                type: 'JSSlot',
+                title: '头部附加',
+                value: [],
+              },
+            },
+          },
+          {
+            name: 'footer',
+            title: '底部',
+            setter: {
+              componentName: 'SlotSetter',
+              initialValue: {
+                type: 'JSSlot',
+                title: '底部',
+                value: [],
+              },
+            },
+          },
+          {
+            name: 'action',
+            title: '操作区',
+            setter: {
+              componentName: 'SlotSetter',
+              initialValue: {
+                type: 'JSSlot',
+                title: '操作区',
+                value: [],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  snippets: [
+    {
+      title: '卡片',
+      screenshot: cardImage,
+      schema: {
+        componentName: 'NCard',
+        props: {
+          title: 'Default size card',
+        },
+      },
+    },
+  ],
 };
 
 export default meta;
