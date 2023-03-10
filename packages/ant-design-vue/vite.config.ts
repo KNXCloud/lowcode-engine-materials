ecfe9dbad0b407a41e1afd7d8ffa79ef283df928
knight.chen <knightchen@knx.com.cn>
2023-03-11 00:49:08 +0800

    misc: upgrade packages verion

--- packages/ant-design-vue/vite.config.ts
+++ packages/ant-design-vue/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      formats: ['es'],
+      formats: ['es', 'cjs'],
     },
     emptyOutDir: false,
     rollupOptions: {
